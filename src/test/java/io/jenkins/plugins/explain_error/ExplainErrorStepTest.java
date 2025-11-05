eec0f3077a733f1ba1d33d25d07ca6d530478a49
Markus Winter <m.winter@sap.com>
2025-11-05 22:17:56 +0100

    improve ExplainErrorStep tests

--- src/test/java/io/jenkins/plugins/explain_error/ExplainErrorStepTest.java
+++ src/test/java/io/jenkins/plugins/explain_error/ExplainErrorStepTest.java
@@ -1,6 +1,9 @@
 package io.jenkins.plugins.explain_error;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import io.jenkins.plugins.explain_error.provider.OpenAIProvider;
+import io.jenkins.plugins.explain_error.provider.TestProvider;
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
 import org.jenkinsci.plugins.workflow.job.WorkflowRun;
@@ -12,7 +15,7 @@ import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 class ExplainErrorStepTest {
 
     @Test
-    void testExplainErrorStep(JenkinsRule jenkins) throws Exception {
+    void testExplainErrorStepInvalidConfig(JenkinsRule jenkins) throws Exception {
         GlobalConfigurationImpl config = GlobalConfigurationImpl.get();
         config.setAiProvider(new OpenAIProvider(null, "test-model", null));
 
@@ -32,4 +35,26 @@ class ExplainErrorStepTest {
         // Check that the explain error step was called and logged the expected error
         jenkins.assertLogContains("ERROR: The provider is not properly configured.", run);
     }
+
+    @Test
+    void testExplainErrorStep(JenkinsRule jenkins) throws Exception {
+        GlobalConfigurationImpl config = GlobalConfigurationImpl.get();
+        config.setAiProvider(new TestProvider());
+
+        // Create a test pipeline job
+        WorkflowJob job = jenkins.createProject(WorkflowJob.class, "test-explain-error");
+
+        // Define a simple pipeline that calls explainError directly
+        String pipelineScript = "node {\n"
+                + "    explainError()\n"
+                + "}";
+
+        job.setDefinition(new CpsFlowDefinition(pipelineScript, true));
+
+        // Run the job - it should succeed but log the API key error
+        WorkflowRun run = jenkins.assertBuildStatus(hudson.model.Result.SUCCESS, job.scheduleBuild2(0));
+        ErrorExplanationAction action = run.getAction(ErrorExplanationAction.class);
+        assertNotNull(action);
+    }
+
 }
